05ecd4466fefcea8e8fa420068cddda36a983f10
eemin90 <eemin90@naver.com>
2021-03-25 14:21:25 +0900

    2장 Type

---
+++ src/p02/lecture/A03Type.java
@@ -0,0 +1,19 @@
+package p02.lecture;
+
+public class A03Type {
+	public static void main(String[] args) {
+		// javascript
+		// var a = 3; // number
+		// a = "hello"; // string
+		// a = true; // boolean
+		
+		// java 변수 선언시 타입을 결정해야 함
+		// type: 기본타입(8개), 참조타입(챕터 5)
+		// 기본타입 8개: byte, short, int, long, boolean, char, float, double
+		// 참조타입: 기본타입이 아닌 모두
+		
+		int a;
+		a = 3; // ok
+//		a = true; // not ok
+	}
+}
---
+++ src/p02/lecture/A03TypeByte.java
@@ -0,0 +1,7 @@
+package p02.lecture;
+
+public class A03TypeByte {
+	public static void main(String[] args) {
+		
+	}
+}
