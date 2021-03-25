63ecbafb4026024823dea08941bae1fa85c873cc
eemin90 <eemin90@naver.com>
2021-03-25 10:29:57 +0900

    first commit

---
+++ src/p01/Comment.java
@@ -0,0 +1,19 @@
+package p01;
+
+public class Comment {
+	public static void main(String[] args) {
+
+		// sout ctrl+space로 System.out.println(); 바로 출력
+		System.out.println("test");
+
+		System.out.println("주석");
+
+		// 한 줄 주석은 slash 2개
+		System.out.println("한 줄 주석");
+
+		/*
+		 * 여러 줄 주석은 slash* *slash 사이에 작성
+		 */
+		System.out.println("여러 줄 주석");
+	}
+}
