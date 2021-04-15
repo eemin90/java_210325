6dab40c0b5ced316508848285dfe6cf293c2ba2a
eemin90 <eemin90@naver.com>
2021-04-15 16:02:13 +0900

    8장 인터페이스

---
+++ src/p08/lecture/ex6/A01Anonymous.java
@@ -0,0 +1,21 @@
+package p08.lecture.ex6;
+
+public class A01Anonymous {
+	public static void main(String[] args) {
+		MyInterface i1;
+		i1 = new MyClass();
+		
+		// 클래스와 인스턴스를 한 번에 만듬
+		// 무명 클래스(anonymous class). 익명 구현 객체(클래스)
+		MyInterface i2;
+		i2 = new MyInterface() {
+			@Override
+			public void method1() {
+				System.out.println("anonymous class method1");
+			}
+		};
+		
+		i1.method1();
+		i2.method1();
+	}
+}
