131a68e680fa18139dcd135dd9e5fe4ea897e9b1
eemin90 <eemin90@naver.com>
2021-04-20 17:02:19 +0900

    10장 예외 처리

--- src/p10/textbook/s100701/Account.java
+++ src/p10/textbook/s100701/Account.java
@@ -13,8 +13,10 @@ public class Account {
 		balance += money;
 	}
 	
+	// 사용자 정의 예외 떠넘기기
 	public void withdraw(int money) throws BalanceInsufficientException {
 		if (balance < money) {
+			// 사용자 정의 예외 발생
 			throw new BalanceInsufficientException("잔고부족: " + (money - balance) + "모자람");
 		}
 		balance -= money;
