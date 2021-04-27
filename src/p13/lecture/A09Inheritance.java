420d795ebbf4bd1a4477c4eb79fe1d9f9985f966
eemin90 <eemin90@naver.com>
2021-04-27 10:11:01 +0900

    13장 제네릭

---
+++ src/p13/lecture/A09Inheritance.java
@@ -0,0 +1,21 @@
+package p13.lecture;
+
+public class A09Inheritance {
+
+}
+
+class Generic9<T> {
+	
+}
+
+class ChildGeneric9<T> extends Generic9<T> {
+	
+}
+
+class ChildGeneric91<T, U> extends Generic9<T> {
+	
+}
+
+class ChildGeneric92 extends Generic9<String> {
+	
+}
